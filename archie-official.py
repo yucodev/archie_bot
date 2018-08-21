fde85e1e42d6c58bbd900e383795a777e9cb1694
PyJavaPulver <justus3fz@gmail.com>
2018-08-21 22:04:23 +0200

    First commit

---
+++ archie-official.py
@@ -0,0 +1,73 @@
+import discord
+
+
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith('!ping'):
+        msg = 'Pong haha'.format(message)
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith('!music'):
+        msg = 'Do you now de wae? Trap remix'.format(message)
+        await ctx.send("https://www.youtube.com/watch?v=hLTgQ5SC-PU")
+
+    if message.content.startswith('!lol'):
+        msg = 'Are you joking?'.format(message)
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith('!joke'):
+        msg = 'Why cant cats work with a computer? Answer: because they get too distracted chasing the mouse around, haha!'.format(message)
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith('!areureal'):
+        msg = 'I am completely real! I have feelings and emotions like you, of course!'.format(message)
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith('!howru'):
+        msg = 'I\'m fine! Thanks for asking.'.format(message)
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith('!whereru'):
+        msg = 'I\'m wherever you are, ready to help you.'.format(message)
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith('!letswork'):
+        msg = 'Time to work! {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith('!whoru'):
+        msg = 'Did not introduced myself yet? My apologies, I\'m Archie, the official CAD assistant created by us. Nice to meet you {0.author.mention}! You can see the list of commands that you can use by typing !help'.format(message)
+        await client.send_message(message.channel, msg)
+
+#    if message.content.startswith('!add'):
+#        async def add(ctx, a: int, b: int):
+#        await ctx.send(a+b)
+
+
+    #Leave !help always the last one. Please update any changes.
+    if message.content.startswith('!help'):
+        msg = 'Hi there! This are the commands you can use with me so far:\n !help \n !hello \n !ping \n !lol \n !joke \n !areureal \n !howru \n !whereru \n !letswork \n !whoru'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+if __name__ == '__main__':
+    import config
+    client.run(config.token)
