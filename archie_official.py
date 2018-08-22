08dd501899943e949cc43c67bfda780a7a38a246
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2018-08-22 19:21:40 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -17,11 +17,11 @@ async def on_message(message):
         return
     
     if message.content.startswith('!me'):
-        msg = 'The users name is: {}'.format(user.name))
-        msg = 'The users ID is: {}'.format(user.id))
-        msg = 'The users status is: {}'.format(user.status))
-        msg = 'The users highest role is: {}'.format(user.top_role))
-        msg = 'The user joined at: {}'.format(user.joined_at))
+        msg = 'The users name is: {}'.format(user.name)
+        msg = 'The users ID is: {}'.format(user.id)
+        msg = 'The users status is: {}'.format(user.status)
+        msg = 'The users highest role is: {}'.format(user.top_role)
+        msg = 'The user joined at: {}'.format(user.joined_at)
         await client.send_message(message.channel, msg)
     
     if message.content.startswith('!hello'):
