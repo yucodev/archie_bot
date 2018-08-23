5859c4fcfc0c1b2c8f69bb6ae7dd81ccf5848f74
Viktaur <victorgabal@gmail.com>
2018-08-23 17:11:35 +0200

    Update

--- simplebot.py
+++ simplebot.py
@@ -3,16 +3,12 @@ import discord
 client = discord.Client()
 
 @client.event
-async def on_message():
-    print('Archie here!')
-
-@client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
-
+    await client.send_message(discord.Object(id='453629533569024017'), 'Archie here!')
 
 if __name__ == '__main__':
     import config
