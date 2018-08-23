62d58c3b10247e6a0e3890640f32d72171106758
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2018-08-23 15:27:21 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -126,7 +126,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    await client.send_message(discord.Object(id='458378197478932492'), 'Archie is now online!')
+    await client.send_message(discord.Object(id='481951758722138113'), 'Archie is now online!')
     await client.change_presence(game=discord.Game(name="CAD Developers | !help"))
     
     
