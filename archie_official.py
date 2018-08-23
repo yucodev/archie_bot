c9dc0cefaab98521d5ee9af0a04d301e8a6dc2eb
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2018-08-23 14:04:43 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -16,7 +16,13 @@ async def on_message(message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
-
+    
+   if message.content.startswith('!jokes'):
+        async def randommessage():
+        response = random.choice(["one", "two", "three"])
+        await bot.say(response)
+        await asyncio.sleep(120)
+    
     if message.content.startswith('!myid'):
         msg = 'Your user ID is: {0.author.id}'.format(message)
         await client.send_message(message.channel, msg)
