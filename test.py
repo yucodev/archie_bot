24e4c0e9eedf30cd78d020c3e2f7b3c1285adc47
Viktaur <victorgabal@gmail.com>
2018-08-23 23:38:45 +0200

    U

--- test.py
+++ test.py
@@ -1,10 +1,26 @@
-import bot
+from os import getenv
+import discord
+from discord.ext import commands
 
-@Bot.command(pass_context=True)
-async def random1and10(ctx, number):
-    try:
-        arg = random.randint(1, int(number))
-    except ValueError:
-        await Bot.say("Invalid number")
-    else:
-        await Bot.say(str(arg))
+description = 'A nice little event bot'
+bot = commands.Bot(command_prefix='!', description=description)
+
+
+@bot.event
+async def on_ready():
+    print(bot.user.id)
+    print(bot.user.name)
+    print('---------------')
+    print('This bot is ready for action!')
+
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+    '''Returns pong when called'''
+    author = ctx.message.author.name
+    server = ctx.message.server.name
+    await bot.say('Pong for {} from {}!'.format(author, server))
+
+if __name__ == '__main__':
+    import config
+    bot.run(config.token)
