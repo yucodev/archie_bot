7a0cf67c0083d75dd6e1f19129883202838b3942
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2018-08-25 22:45:13 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -163,28 +163,27 @@ async def on_message(message):
 #             await client.add_reaction(message, '🌕')
 
 @commands.command(pass_context=True)
-    async def translate(self, ctx, to_language, *, msg):
+async def translate(self, ctx, to_language, *, msg):
         #"""Translates words from one language to another. Do [p]help translate for more information.
         #Usage:
         #[p]translate <new language> <words> - Translate words from one language to another. Full language names must be used.
         #The original language will be assumed automatically.
-        """
         await ctx.message.delete()
-        if to_language == "rot13":  # little easter egg
+    if to_language == "rot13":  # little easter egg
             embed = discord.Embed(color=discord.Color.blue())
             embed.add_field(name="Original", value=msg, inline=False)
             embed.add_field(name="ROT13", value=codecs.encode(msg, "rot_13"), inline=False)
             return await ctx.send("", embed=embed)
-        async with self.bot.session.get("https://gist.githubusercontent.com/astronautlevel2/93a19379bd52b351dbc6eef269efa0bc/raw/18d55123bc85e2ef8f54e09007489ceff9b3ba51/langs.json") as resp:
+            async with self.bot.session.get("https://gist.githubusercontent.com/astronautlevel2/93a19379bd52b351dbc6eef269efa0bc/raw/18d55123bc85e2ef8f54e09007489ceff9b3ba51/langs.json") as resp:
             lang_codes = await resp.json(content_type='text/plain')
         real_language = False
         to_language = to_language.lower()
         for entry in lang_codes:
-            if to_language in lang_codes[entry]["name"].replace(";", "").replace(",", "").lower().split():
+    if to_language in lang_codes[entry]["name"].replace(";", "").replace(",", "").lower().split():
                 language = lang_codes[entry]["name"].replace(";", "").replace(",", "").split()[0]
                 to_language = entry
                 real_language = True
-        if real_language:
+    if real_language:
             async with self.bot.session.get("https://translate.google.com/m",
                                         params={"hl": to_language, "sl": "auto", "q": msg}) as resp:
                 translate = await resp.text()
@@ -193,7 +192,7 @@ async def on_message(message):
             embed = discord.Embed(color=discord.Color.blue())
             embed.add_field(name="Original", value=msg, inline=False)
             embed.add_field(name=language, value=result.replace("&amp;", "&"), inline=False)
-            if result == msg:
+    if result == msg:
                 embed.add_field(name="Warning", value="This language may not be supported by Google Translate.")
             await ctx.send("", embed=embed)
         else:
