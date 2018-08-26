60316e78130efd67aec9cc13afc9475a55e0bce0
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2018-08-26 15:18:55 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -21,7 +21,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('archie'):
+    if message.content.contains('archie'):
         msg = 'What do you want?'.format(message)
         await client.send_message(message.channel, msg)
        
