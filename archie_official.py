06e11bfce9c8c374b57c2def315181722e0de002
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2018-08-26 15:26:48 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -71,7 +71,7 @@ async def on_message(message):
         msg = 'Current date and time: ' + str(datetime.datetime.now())
         await client.send_message(message.channel, msg)
 
-    if message.content.startswith('!hello'):
+    if ('hello archie') in message.content:
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
 
