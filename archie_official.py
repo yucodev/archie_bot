0145a08fe3cc0714059ee45cba57edef281eab65
viktaur <30535579+viktaur@users.noreply.github.com>
2018-08-29 12:35:17 +0200

    U

--- archie_official.py
+++ archie_official.py
@@ -81,14 +81,6 @@ async def on_message(message):
         msg = 'My recommendation for {0.author.mention}: '.format(message) + random.choice([a, b, c, d, e, f, g])
         await client.send_message(message.channel, msg)
 
-    if ('pepe') in message.content:
-        a = 'El que te la mete hasta por el retrete!'
-        b = 'El que te mete el Nepe!'
-        c = 'El que te la saca y te la mete!'
-        d = 'El que hasta los huevos me mete'
-        msg = 'Pepe, '.format(message) + random.choice([a, b, c, d])
-        await client.send_message(message.channel, msg)
-
     if message.content.startswith('!lal'):
         msg = 'Are you jaking?'.format(message)
         await client.send_message(message.channel, msg)
