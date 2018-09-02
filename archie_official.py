44e2318ccd5a1cd25301610f87b0545b51964aa0
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2018-09-02 16:27:48 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -27,7 +27,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!myid'):
+  if message.content.startswith('!myid'):
         msg = 'Your user ID is: {0.author.id}'.format(message)
         await client.send_message(message.channel, msg)
 
