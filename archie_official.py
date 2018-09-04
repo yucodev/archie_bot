4abae6599357a1c699c6696adec81323cb6373ea
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2018-09-04 22:24:37 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -23,10 +23,10 @@ async def on_message(message):
         return
 
     if message.content.startswith('!randommember'):
-        a = '<@427204692234469387>'
-        b = '<@334252448036159488>'
-        c = '<@367958242980003850>'
-        d = '<@427414654994612224>'
+        a = '@manersat'
+        b = '@viktaur'
+        c = '@pupspulver05'
+        d = '@TheAlx1Boy'
         msg = random.choice([a, b, c, d]).format(message)
         await client.send_message(message.channel, msg)
 
