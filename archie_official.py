6aaf12952390ab1e982b2f1fcdf0f8b734590a0c
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2018-09-06 15:50:36 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -44,7 +44,7 @@ async def on_message(message):
         msg = 'WEATHER FORECAST GIJON'.format(message)
         await client.send_message(message.channel, msg)
         time.sleep(1)
-        for forecast in forecasts:
+        #for forecast in forecasts:
             msg = 'On ' + forecast.date.format(message)
             await client.send_message(message.channel, msg)
             msg = '- ' + forecast.text.format(message)
