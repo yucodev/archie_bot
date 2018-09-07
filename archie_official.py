75d8e5196bcb7a420e5bf50eb0bdf176d0d7c9c9
viktaur <30535579+viktaur@users.noreply.github.com>
2018-09-07 14:17:15 +0200

    U

--- archie_official.py
+++ archie_official.py
@@ -28,9 +28,9 @@ async def on_message(message):
 
     if message.content.startswith('!weather'):
         weather = Weather(unit=Unit.CELSIUS)
-		await client.send_message(message.channel, 'What city?')
+        await client.send_message(message.channel, 'What city?')
         response = client.wait_for_message(author=ctx.message.author, timeout=30)
-	    location = weather.lookup_by_location(reponse.content)
+        location = weather.lookup_by_location(reponse.content)
         await client.send_message(message.author, '**_WEATHER FORECAST ' + response.content.upper() + ' _**')
         msg = 'Forecast sent per DM'
         await client.send_message(message.channel, msg)
