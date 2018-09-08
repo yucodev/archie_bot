ca937561df761b76286fe0293bb0da7f7608cee3
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2018-09-08 22:37:18 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -78,9 +78,10 @@ async def on_message(message):
         await client.send_message(message.channel, msg)
 
     if message.content.startswith('!quiz'):
+        response = message.content.split(" ")
         msg = 'Can cats fly? (yes/no)'
         await client.send_message(message.channel, msg)
-        if (response == 'no')
+        elif response == 'no'
             await client.send_message(message.channel, 'Noooooo')
             time.sleep(2)
             await client.send_message(message.channel, 'But they should... :heart_eyes_cat:')
