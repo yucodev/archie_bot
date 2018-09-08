d507a5b75b2866e77826cdf1662031a1f699bbdc
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2018-09-08 23:10:04 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -86,7 +86,7 @@ async def on_message(message):
          #   await client.send_message(message.channel, 'Noooooo')
           #  time.sleep(2)
            # await client.send_message(message.channel, 'But they should... :heart_eyes_cat:')
-        if 'split' == 'response'
+        
             print("lol")
             await client.send_message(message.channel, 'Of course they can!')
             time.sleep(2)
