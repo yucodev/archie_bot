610d2ee868378b2f54f1af9215bc19d4ea9cbdb9
viktaur <30535579+viktaur@users.noreply.github.com>
2018-09-12 00:17:53 +0200

    U

--- archie_official.py
+++ archie_official.py
@@ -393,7 +393,7 @@ async def on_message(message):
 		msg = 'What kind of sport?, {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
 
-    if ('a bit of everithing') in message.content:
+    if ('a bit of everything') in message.content:
         msg = 'That\'s very good'.format(message)
         await client.send_message(message.channel, msg)
 
