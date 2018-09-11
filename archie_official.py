498a4f5e9f8b2a0ecb14129cd65f7dd06dc46ff9
viktaur <30535579+viktaur@users.noreply.github.com>
2018-09-11 23:46:14 +0200

    U

--- archie_official.py
+++ archie_official.py
@@ -383,7 +383,7 @@ async def on_message(message):
         msg = 'I\'m fine too :yum:'.format(message)
         await client.send_message(message.channel, msg)
         time.sleep(2)
-        msg = 'what are you going to do today'.format(message)
+        msg = 'What are you going to do today, {0.author.mention}?'.format(message)
         await client.send_message(message.channel, msg)
 
     if ('not fine') in message.content:
