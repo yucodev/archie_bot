2a0739511daa4e5c917d9e2327b8bc5d74142fb4
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2018-09-13 17:36:02 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -45,9 +45,9 @@ async def on_message(message):
         time.sleep(1)
         for forecast in forecasts:
             await client.send_message(message.author, '**On ' + forecast.date + '**')
-            await client.send_message(message.author, ' :low_brightness: ' + forecast.text)
-            await client.send_message(message.author, ' :small_orange_diamond: Max temp. ' + forecast.high)
-            await client.send_message(message.author, ' :small_blue_diamond: Min temp. ' + forecast.low)
+            #await client.send_message(message.author, ' :low_brightness: ' + forecast.text)
+            #await client.send_message(message.author, ' :small_orange_diamond: Max temp. ' + forecast.high)
+            #await client.send_message(message.author, ' :small_blue_diamond: Min temp. ' + forecast.low)
             if ('today') in message.content:
                 print('today detectado brooooooo')
                 break
