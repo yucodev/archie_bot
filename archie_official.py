531efb5533b359e48a6d8d522a4e8494138e9935
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2018-09-13 17:32:50 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -48,10 +48,12 @@ async def on_message(message):
             await client.send_message(message.author, ' :low_brightness: ' + forecast.text)
             await client.send_message(message.author, ' :small_orange_diamond: Max temp. ' + forecast.high)
             await client.send_message(message.author, ' :small_blue_diamond: Min temp. ' + forecast.low)
-            await client.send_message(message.author, ' --------------------- ')  
             if ('today') in message.content:
                 print('today detectado brooooooo')
-                return
+                break
+            await client.send_message(message.author, ' --------------------- ')  
+
+                
 
     if message.content.startswith('!echo'):
         echo = message.content.split(" ")
