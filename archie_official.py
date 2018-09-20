5f8f396be072f1aec31d0b6bebd8b0f723a289be
viktaur <30535579+viktaur@users.noreply.github.com>
2018-09-20 21:30:10 +0200

    U

--- archie_official.py
+++ archie_official.py
@@ -136,13 +136,16 @@ async def on_message(message):
         await client.send_message(message.channel, msg)
 
     if message.content.startswith('!joke'):
-        a = 'Can a kangaroo jump higher than a house?' + time.sleep(3) + 'Of course, a house doesn’t jump at all.'
-        b = 'Anton, do you think I’m a bad mother?' + time.sleep(3) + 'My name is Paul.'
-        c = 'Why can\'t cats work with a computer?' + time.sleep(3) + 'Because they get too distracted chasing the mouse around, haha!'
-        d = 'My dog used to chase people on a bike a lot.' + time.sleep(3) + 'It got so bad, finally I had to take his bike away.'
-        e = 'What do Italian ghosts have for dinner?' + time.sleep(3) + 'Spook-hetti!'
-        msg = random.choice([a, b, c, d, e]).format(message)
-        await client.send_message(message.channel, msg)
+        jokes = [
+            ('Can a kangaroo jump higher than a house?', 'Of course, a house doesn’t jump at all.'),
+            ('Anton, do you think I’m a bad mother?', 'My name is Paul.'),
+            ('Why can\'t cats work with a computer?', 'Because they get too distracted chasing the mouse around, haha!'),
+            ('My dog used to chase people on a bike a lot.', 'It got so bad, finally I had to take his bike away.'),
+            ('What do Italian ghosts have for dinner?', 'Spook-hetti!')]
+        setup, punchline = random.choice(jokes)
+        await client.send_message(message.channel, setup)
+        await asyncio.sleep(3)
+        await client.send_message(message.channel, punchline)
 
     # if message.content.startswith('!joke'):
         # a = 'Can a kangaroo jump higher than a house?\n Of course, a house doesn’t jump at all.'
