84ba712909ab19adb8633790cc3011f36018ed50
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2018-09-28 23:52:42 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -58,7 +58,7 @@ async def on_message(message):
         req = requests.get(url, headers=apiKey)
         data = req.json()
         solo_wins = data["stats"]["p2"]["top1"]["valueInt"]
-        await client.send_message(message.channel, 'Solo Wins: ', solo_wins)
+        await client.send_message(message.channel, str('Solo Wins: ') + solo_wins)
         print("Solo Wins:", solo_wins)
     
     if message.content.startswith('!weathercel'):
