ecf4e04070cc1819fd31e967b80078d5b1ecdd0a
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2019-04-19 17:40:18 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -47,8 +47,9 @@ async def on_message(message):
        player = apex.player('pupspulver05')
        #await client.send_message(message.channel, player)
        #for legend in player.legends:
-       await client.send_message(message.channel, legend.name)
+       await client.send_message(message.channel, legend.legend_name)
        await client.send_message(message.channel, legend.icon)
+       await client.send_message(message.channel, player.stats)
        #await client.send_message(message.channel, legend.damage)
            #break
     
