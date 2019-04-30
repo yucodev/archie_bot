cae4fd79e48aa65e928e664fec46e7cdca8716f2
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2019-04-30 19:51:32 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -394,14 +394,24 @@ async def on_message(message):
         msg = random.choice([a, b]).format(message)
         await client.send_message(message.channel, msg)
 
-    if message.content.startswith('!randomnum'):
-        args = message.content.split(" ")
-        num1 = " ".join(args[1:])
-        num2 = " ".join(args[2:])
-        await client.send_message(message.channel, num1)
-        await client.send_message(message.channel, num1)
-        msg = random.randint(num1, int(num2))
-        await client.send_message(message.channel, msg)
+    if message.content.startswith("!randomnum"):
+        await client.send_message(message.channel, "Enter the first number: ")
+        num1 = await client.wait_for_message(int)
+        #numm1 = int(num1)
+        await client.send_message(message.channel, "Enter the second number: ")
+        num2 = await client.wait_for_message(int)
+        numm2 = int(num2)
+        randomnum = random.randint(num1, numm2)
+        await client.send_message(message.channel, randomnum)
+
+    #if message.content.startswith('!randomnum'):
+     #   args = message.content.split(" ")
+     #   num1 = " ".join(args[1:])
+     #   num2 = " ".join(args[2:])
+     #   await client.send_message(message.channel, num1)
+     #   await client.send_message(message.channel, num1)
+     #   msg = random.randint(num1, int(num2))
+     #   await client.send_message(message.channel, msg)
 
     if message.content.startswith('!5random'):
         msg = random.randint(1, int(5))
