c97830bbb15404190483204742cdf6f95defe330
viktaur <victorgabal@gmail.com>
2019-04-30 21:44:10 +0200

    fix bugs

--- archie_official.py
+++ archie_official.py
@@ -396,10 +396,10 @@ async def on_message(message):
 
     if message.content.startswith('!randomnum'):
         args = message.content.split(" ")
-        num1 = str(" ".join(args[1:]))
-        num2 = str(" ".join(args[2:]))
+        num1 = (" ".join(args[1:]))
+        num2 = (" ".join(args[2:]))
         await client.send_message(message.channel, num1)
-        await client.send_message(message.channel, num1)
+        await client.send_message(message.channel, num2)
         #msg = random.randint(num1, int(num2))
         #await client.send_message(message.channel, msg)
 
