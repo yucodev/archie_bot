efe898555dcbbe30871ee38ce605fb47e1938cf9
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2019-04-30 19:35:56 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -398,6 +398,7 @@ async def on_message(message):
         args = message.content.split(" ")
         num1 = " ".join(args[1])
         num2 = " ".join(args[2])
+        await client.send_message(message.channel, num1, num2)
         msg = random.randint(num1, int(num2))
         await client.send_message(message.channel, msg)
 
