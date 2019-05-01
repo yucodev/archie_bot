174e0b6646d170845961a9b0827b2ffd9a7d7693
viktaur <30535579+viktaur@users.noreply.github.com>
2019-05-01 14:09:35 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -394,14 +394,14 @@ async def on_message(message):
         msg = random.choice([a, b]).format(message)
         await client.send_message(message.channel, msg)
 
-    # if message.content.startswith('!randomnum'):
-    #     args = message.content.split(" ")
-    #     num1 = int(" ".join(args[1]))
-    #     num2 = int(" ".join(args[2]))
-    #     # msg1 = num1, num2
-    #     # await client.send_message(message.channel, msg1)
-    #     msg = random.randint(num1, num2)
-    #     await client.send_message(message.channel, msg)
+    if message.content.startswith('!randomnum'):
+        args = message.content.split(" ")
+        num1 = int(" ".join(args[1]))
+        num2 = int(" ".join(args[2]))
+        # msg1 = num1, num2
+        # await client.send_message(message.channel, msg1)
+        msg = random.randint(num1, num2)
+        await client.send_message(message.channel, msg)
 
     # elif message.content.startswith("!random"):
     #     await client.send_message(message.channel, "Enter a number: ")
