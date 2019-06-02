0e7665a8d35ba450ceeadeb867300b298a91d9de
viktaur <30535579+viktaur@users.noreply.github.com>
2019-06-02 21:25:14 +0200

    Update archie_official.py

--- archie_official.py
+++ archie_official.py
@@ -100,10 +100,6 @@ async def on_message(message):
         msg = 'Last Fortnite news: https://www.epicgames.com/fortnite/en/news'
         await client.send_message(message.channel, msg)
 
-    if message.content.startswith('!royalbaby'):
-        msg = 'Hi @everyone! The Dukes of Sussex have officially anounce the Royal Baby name. Guess what! He\'s called Archie, like me! :smiley::blush:'
-        await client.send_message(discord.Object(id=488783225913868321), msg)
-
     #### Weather yahoo API is no longer available ####
 
     #if message.content.startswith('!weathercel'):
