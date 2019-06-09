e109451513a70cc72b532fd58e06081eb997593c
viktaur <30535579+viktaur@users.noreply.github.com>
2019-06-09 17:55:44 +0200

    U

--- tests/gravitation.py
+++ tests/gravitation.py
@@ -2,46 +2,50 @@ import math
 import discord
 import asyncio
 import random
-
+# import sys
 
 
 
 # DATA #
-G = 6.67 * 10**-11
-earth_mass = 5.97 * 10**24
-moon_mass = 7.34 * 10**22
+G = 6.67e-11
+earth_mass = 5.97e+24
+moon_mass = 7.34e+22
 earth_radius = 6371000
-earth_moon_distance = 384400000
+earth_moon_distance = 3.844e+8
+earth_sun_distance = 1.496e+11
+average_person_mass = 70
 
 
 
-# DISPLAY SCRIPT #
+# INPUT SCRIPT #
 print('* * * * * * * * * * * * * * * * * * * * * * * *')
 print('* G R A V I T A T I O N   C A L C U L A T O R *')
 print('* * * * * * * * * * * * * * * * * * * * * * * *')
 print(' ')
 print('Type --help after the execution command (in a new line) to read the instructions and how to use the variables.')
 input('Press any key to continue...')
-print('Enter the bigger mass (kg): ')
+print('Insert the bigger mass (kg): ')
 M = eval(input())
-print('Enter the smaller mass (kg): ')
+print('Insert the smaller mass (kg): ')
 m = eval(input())
-print('Enter the distance of the objects or the radius of the orbit (m): ')
+print('Insert the distance of the objects or the radius of the orbit (m): ')
 R = eval(input())
-# DISPLAY SCRIPT #
 
 
 
 # CALCULATIONS AND FORMULES #
 g = G * M / (R**2)
-P = m * g
 a = G * m / (R**2)
+P = m * g
+P2 = M * a * (-1)
 v = math.sqrt(G * M / R)
 T = (2 * math.pi * R) / v
 uu = v / R     # or (2 * math.pi) / T
 
 
 
+
+# DISPLAY SCRIPT #
 print(' ')
 print('INPUT')
 print('M = ' + str(M) + ' kg')
@@ -49,7 +53,8 @@ print('m = ' + str(m) + ' kg')
 print('R = ' + str(R) + ' m')
 print(' ')
 print('OUTPUT')
-print('P = ' + str(P) + ' N')
+print('P (M on m) = ' + str(P) + ' N')
+print('P (m on M) = ' + str(P2) + ' N')
 print('g = ' + str(g) + ' m/s2')
 print('a = ' + str(a) + ' m/s2')
 print('T = ' + str(T) + ' s')
---
+++ tests/inclined-plane.py
@@ -0,0 +1,57 @@
+import math
+import discord
+import asyncio
+import random
+
+
+
+# DATA #
+earth_gravity = 9.80665
+
+
+# INPUT SCRIPT #
+print('* * * * * * * * * * * * * * * * * * * * * * * * * * *')
+print('* I N C L I N E D   P L A N E   C A L C U L A T O R *')
+print('* * * * * * * * * * * * * * * * * * * * * * * * * * *')
+print(' ')
+print('Type --help after the execution command (in a new line) to read the instructions and how to use the variables.')
+input('Press any key to continue...')
+print('Insert object mass (kg): ')
+m = eval(input())
+print('Insert gravity (m/s²): ')
+g = eval(input())
+print('Insert plane\'s inclination angel (DEG): ')
+Â = eval(input())
+print('Insert coefficient of friction: ')
+µ = eval(input())
+
+
+
+
+# CALCULATIONS AND FORMULES #
+P = m * g
+Px = m * g * math.sin(math.radians(Â))
+Py = m * g * math.cos(math.radians(Â))
+N = -Py
+Fƒ = µ * N
+Fres = Px - Fƒ
+a = Fres / m
+
+
+
+# DISPLAY SCRIPT #
+print(' ')
+print('INPUT')
+print('m = ' + str(m) + ' kg')
+print('g = ' + str(g) + ' m/s²')
+print('Â = ' + str(Â) + 'º')
+print('µ = ' + str(µ))
+print(' ')
+print('OUTPUT')
+print('P = ' + str(P) + ' N')
+print('Px = ' + str(Px) + ' N')
+print('Py = ' + str(Py) + ' N')
+print('N = ' + str(N) + ' N')
+print('Fƒ = ' + str(Fƒ) + ' N')
+print('Fres = ' + str(Fres) + ' N')
+print('a (Fres) = ' + str(a) + ' m/s²')
