2d096d68ff47e81376bb8d2123c9b4ab033bbbe8
viktaur <victorgabal@gmail.com>
2019-06-13 16:06:33 +0200

    u

--- tests/inclined-plane.py
+++ tests/inclined-plane.py
@@ -15,9 +15,11 @@ earth_gravity = data.earth_gravity #9.80665
 print('* * * * * * * * * * * * * * * * * * * * * * * * * * *')
 print('* I N C L I N E D   P L A N E   C A L C U L A T O R *')
 print('* * * * * * * * * * * * * * * * * * * * * * * * * * *')
+time.sleep(1)
 print(' ')
+time.sleep(0.1)
 print(' ')
-time.sleep(2)
+time.sleep(0.1)
 print('                                                    ***')
 time.sleep(0.1)
 print('                                                 ******')
@@ -62,46 +64,75 @@ print(' ')
 time.sleep(0.1)
 print(' ')
 time.sleep(1)
-print('Type --help after the execution command (in a new line) to read the instructions and how to use the variables.')
-time.sleep(1)
-input('Press any key to continue...')
-time.sleep(0.5)
-print('Insert object mass (kg): ')
-m = eval(input())
-print('Insert gravity (m/s²): ')
-g = eval(input())
-print('Insert plane\'s inclination angel (DEG): ')
-Â = eval(input())
-print('Insert coefficient of friction: ')
-µ = eval(input())
+
+print('Type "S" to start calculator or "H" for help or information. Then press enter')
+varinput = input()
+
+if varinput == 'S' or 's':
+    print('Insert object mass (kg): ')
+    m = eval(input())
+    print('Insert gravity (m/s²): ')
+    g = eval(input())
+    print('Insert plane\'s inclination angel (DEG): ')
+    Â = eval(input())
+    print('Insert coefficient of friction: ')
+    µ = eval(input())
+
+    # CALCULATIONS AND FORMULES #
+    P = m * g
+    Px = m * g * math.sin(math.radians(Â))
+    Py = m * g * math.cos(math.radians(Â))
+    N = -Py
+    Fƒ = µ * N
+    Fres = Px + Fƒ
+    a = Fres / m
+
+    # DISPLAY SCRIPT #
+    print(' ')
+    print('INPUT')
+    print('m = ' + str(m) + ' kg')
+    print('g = ' + str(g) + ' m/s²')
+    print('Â = ' + str(Â) + 'º')
+    print('µ = ' + str(µ))
+    print(' ')
+    print('OUTPUT')
+    print('P = ' + str(P) + ' N')
+    print('Px = ' + str(Px) + ' N')
+    print('Py = ' + str(Py) + ' N')
+    print('N = ' + str(N) + ' N')
+    print('Fƒ = ' + str(Fƒ) + ' N')
+    print('Fres = ' + str(Fres) + ' N')
+    print('a (Fres) = ' + str(a) + ' m/s²')
+
+if varinput == 'H' or 'h':
+    print('hello')
 
 
 
-
-# CALCULATIONS AND FORMULES #
-P = m * g
-Px = m * g * math.sin(math.radians(Â))
-Py = m * g * math.cos(math.radians(Â))
-N = -Py
-Fƒ = µ * N
-Fres = Px + Fƒ
-a = Fres / m
-
-
-
-# DISPLAY SCRIPT #
-print(' ')
-print('INPUT')
-print('m = ' + str(m) + ' kg')
-print('g = ' + str(g) + ' m/s²')
-print('Â = ' + str(Â) + 'º')
-print('µ = ' + str(µ))
-print(' ')
-print('OUTPUT')
-print('P = ' + str(P) + ' N')
-print('Px = ' + str(Px) + ' N')
-print('Py = ' + str(Py) + ' N')
-print('N = ' + str(N) + ' N')
-print('Fƒ = ' + str(Fƒ) + ' N')
-print('Fres = ' + str(Fres) + ' N')
-print('a (Fres) = ' + str(a) + ' m/s²')
+# # CALCULATIONS AND FORMULES #
+# P = m * g
+# Px = m * g * math.sin(math.radians(Â))
+# Py = m * g * math.cos(math.radians(Â))
+# N = -Py
+# Fƒ = µ * N
+# Fres = Px + Fƒ
+# a = Fres / m
+#
+#
+#
+# # DISPLAY SCRIPT #
+# print(' ')
+# print('INPUT')
+# print('m = ' + str(m) + ' kg')
+# print('g = ' + str(g) + ' m/s²')
+# print('Â = ' + str(Â) + 'º')
+# print('µ = ' + str(µ))
+# print(' ')
+# print('OUTPUT')
+# print('P = ' + str(P) + ' N')
+# print('Px = ' + str(Px) + ' N')
+# print('Py = ' + str(Py) + ' N')
+# print('N = ' + str(N) + ' N')
+# print('Fƒ = ' + str(Fƒ) + ' N')
+# print('Fres = ' + str(Fres) + ' N')
+# print('a (Fres) = ' + str(a) + ' m/s²')
