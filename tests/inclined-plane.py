cbc382b136082d9b09ff77f8d37e1e4d542f66eb
viktaur <victorgabal@gmail.com>
2019-06-14 18:17:04 +0200

    Update colors

--- tests/inclined-plane.py
+++ tests/inclined-plane.py
@@ -5,6 +5,17 @@ import random
 import data
 import time
 
+# CONSOLE COLORS #
+Black="\033[0;30m"        # Black
+Red="\033[0;31m"          # Red
+Green="\033[0;32m"        # Green
+Yellow="\033[0;33m"       # Yellow
+Blue="\033[0;34m"         # Blue
+Purple="\033[0;35m"       # Purple
+Cyan="\033[0;36m"         # Cyan
+White="\033[0;37m"        # White
+Color_Off="\033[0m"       # Text Reset
+
 
 # DATA #
 earth_gravity = data.earth_gravity #9.80665
@@ -89,7 +100,7 @@ if varinput == 'S' or 's':
 
     # DISPLAY SCRIPT #
     print(' ')
-    print('INPUT')
+    print(Green + 'INPUT' + Color_Off)
     print('m = ' + str(m) + ' kg')
     print('g = ' + str(g) + ' m/s²')
     print('Â = ' + str(Â) + 'º')
