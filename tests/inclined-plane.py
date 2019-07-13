831fd89bbce5722e5198f8bfd01f7321752b9895
viktaur <victorgabal@gmail.com>
2019-07-14 01:20:33 +0200

    U

--- tests/inclined-plane.py
+++ tests/inclined-plane.py
@@ -1,3 +1,4 @@
+from goto import with_goto
 import math
 import discord
 import asyncio
@@ -102,49 +103,67 @@ print(' ')
 time.sleep(0.1)
 print(' ')
 time.sleep(0.1)
-print(' ')
+print(' ' + Color_Off)
 time.sleep(1)
 
-print('Press "S" to start calculator or "H" for help or information. Then press enter.' + Color_Off)
-varinput = input()
+def home():
+    print(Cyan + 'Press "S" to start calculator, "H" for help or information or "E" to exit the program. Then press enter.' + Color_Off)
+    home.varinput = input()
 
-if varinput in ['S', 's']:
-    print(Cyan + 'Insert object mass (kg): ' + Color_Off)
-    m = eval(input())
-    print(Cyan + 'Insert gravity (m/s²): ' + Color_Off)
-    g = eval(input())
-    print(Cyan + 'Insert plane\'s inclination angel (DEG): ' + Color_Off)
-    Â = eval(input())
-    print(Cyan + 'Insert coefficient of friction: ' + Color_Off)
-    µ = eval(input())
+    if home.varinput in ['S', 's', 'START', 'start']:
+        print(Cyan + 'Insert object mass (kg): ' + Color_Off)
+        m = eval(input())
+        print(Cyan + 'Insert gravity (m/s²): ' + Color_Off)
+        g = eval(input())
+        print(Cyan + 'Insert plane\'s inclination angel (DEG): ' + Color_Off)
+        Â = eval(input())
+        print(Cyan + 'Insert coefficient of friction: ' + Color_Off)
+        µ = eval(input())
 
-    # CALCULATIONS AND FORMULES #
-    P = m * g
-    Px = m * g * math.sin(math.radians(Â))
-    Py = m * g * math.cos(math.radians(Â))
-    N = -Py
-    Fƒ = µ * N
-    Fres = Px + Fƒ
-    a = Fres / m
+        # CALCULATIONS AND FORMULES #
+        P = m * g
+        Px = m * g * math.sin(math.radians(Â))
+        Py = m * g * math.cos(math.radians(Â))
+        N = -Py
+        Fƒ = µ * N
+        Fres = Px + Fƒ
+        a = Fres / m
 
-    # DISPLAY SCRIPT #
-    print(' ')
-    print(Cyan + 'INPUT' + Color_Off)
-    print('m = ' + str(m) + ' kg')
-    print('g = ' + str(g) + ' m/s²')
-    print('Â = ' + str(Â) + 'º')
-    print('µ = ' + str(µ))
-    print(' ')
-    print(Cyan + 'OUTPUT' + Color_Off)
-    print('P = ' + str(P) + ' N')
-    print('Px = ' + str(Px) + ' N')
-    print('Py = ' + str(Py) + ' N')
-    print('N = ' + str(N) + ' N')
-    print('Fƒ = ' + str(Fƒ) + ' N')
-    print('Fres = ' + str(Fres) + ' N')
-    print('a (Fres) = ' + str(a) + ' m/s²')
-elif varinput in ['H', 'h']:
-    print(UCyan + 'What is inclined-plane calculator?')
-    print(Cyan + 'This software can calculate almost all forces and some accelerations acting in the inclined plane. You just have to introduce the mass, gravity, inclination angle and coefficient of friction.' + Color_Off)
-else:
-    print('ERROR, ABORTING')
+        # DISPLAY SCRIPT #
+        print(' ')
+        print(Cyan + 'INPUT' + Color_Off)
+        print('m = ' + str(m) + ' kg')
+        print('g = ' + str(g) + ' m/s²')
+        print('Â = ' + str(Â) + 'º')
+        print('µ = ' + str(µ))
+        print(' ')
+        print(Cyan + 'OUTPUT' + Color_Off)
+        print('P = ' + str(P) + ' N')
+        print('Px = ' + str(Px) + ' N')
+        print('Py = ' + str(Py) + ' N')
+        print('N = ' + str(N) + ' N')
+        print('Fƒ = ' + str(Fƒ) + ' N')
+        print('Fres = ' + str(Fres) + ' N')
+        print('a (Fres) = ' + str(a) + ' m/s²')
+        time.sleep(3)
+        print(' ')
+        print(' ')
+        home()
+
+
+    elif home.varinput in ['H', 'h', 'HELP', 'help', 'INFO', 'info']:
+        print(UCyan + 'What is inclined-plane calculator?')
+        print(Cyan + 'This software can calculate almost all forces and some accelerations acting in the inclined plane. You just have to introduce the mass, gravity, inclination angle and coefficient of friction.' + Color_Off)
+        input("Press any key to continue...")
+        home()
+
+    elif home.varinput in ['E', 'e', 'EXIT', 'exit']:
+        print('exit')
+
+    else:
+        print('ERROR, ABORTING')
+
+    return
+
+
+home()
